2bb5b6dda39f17fb0e17e3a667d4caabdf191c23
Felipe C. Barbosa <cavalcantebarbosa@gmail.com>
2024-08-24 22:25:24 -0300

    Obtendo o produto adicionado

---
+++ js/app.js
@@ -0,0 +1,23 @@
+
+
+function adicionar() {
+    // recupear valores nome do produto, quantidade e valor
+    let nomeProduto = document.getElementById('produto');
+    let quantidade = document.getElementById('quantidade');
+    alert(nomeProduto.value);
+    alert(quantidade.value);
+
+
+
+    // calcular o preço, o nosso subtotal
+    // adicionar no carrinho
+    // atualiza o valor total
+
+
+
+
+}
+
+function limpar() {
+
+}
