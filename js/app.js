3f0022559230561be18f77c8657690a1625544ec
Felipe C. Barbosa <cavalcantebarbosa@gmail.com>
2024-08-25 16:01:57 -0300

    Calculando o valor do produto

--- js/app.js
+++ js/app.js
@@ -1,13 +1,16 @@
-
-
 function adicionar() {
     // recupear valores nome do produto, quantidade e valor
-    let nomeProduto = document.getElementById('produto');
+    let produto = document.getElementById('produto').value;
+    let nomeProduto = produto.split('-')[0];
+    let valorUnitario= produto.split('R$')[1];
     let quantidade = document.getElementById('quantidade');
-    alert(nomeProduto.value);
+    alert(nomeProduto);
+    alert(valorUnitario);
     alert(quantidade.value);
 
+    let preco =  quantidade.value * valorUnitario;
 
+    alert(preco);
 
     // calcular o preço, o nosso subtotal
     // adicionar no carrinho
