964d94a5a9d2fdbdfc7da871de5d5a97eaf69423
Felipe C. Barbosa <cavalcantebarbosa@gmail.com>
2024-08-25 16:26:23 -0300

    calculando valor total do carrinho

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+let totalGeral = 0;
+limpar();
+
 function adicionar() {
     // recupear valores nome do produto, quantidade e valor
     let produto = document.getElementById('produto').value;
@@ -10,17 +13,23 @@ function adicionar() {
 
     // adicionar no carrinho
     let carrinho = document.getElementById('lista-produtos');
-    carrinho.innerHTML =  carrinho.innerHTML + `<section class="carrinho__produtos" id="lista-produtos">
+    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos" id="lista-produtos">
                                                     <section class="carrinho__produtos__produto">
                                                     <span class="texto-azul">${quantidade}</span> ${nomeProduto} <span class="texto-azul">R$ ${preco}</span>
                                                     </section>
                                                </section>`;
 
     // atualiza o valor total
-
+    totalGeral = totalGeral + preco;
+    let campoTotal = document.getElementById('valor-total');
+    campoTotal.textContent = `R$ ${totalGeral}`;
+    document.getElementById('quantidade').value = 0;
 }
 
 function limpar() {
+    totalGeral = 0;
+    document.getElementById('valor-total').textContent = 'R$ 0';
+    document.getElementById('lista-produtos').innerHTML = '';
 
 }
 
